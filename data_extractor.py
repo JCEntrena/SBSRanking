3952978a8572433eb5b663db99f9b2e575a24293
JCEntrena <jentrena@correo.ugr.es>
2018-03-16 23:58:49 +0100

    Incluyendo Tier a mano y si el set es de Winners o Losers.

--- data_extractor.py
+++ data_extractor.py
@@ -1,5 +1,7 @@
-import pysmash
-import os, sys
+# -*- coding: utf-8 -*-
+
+import pysmash   # Requiere pip install pysmash. Más información: https://github.com/PeterCat12/pysmash
+import os
 
 if __name__ == '__main__':
 
@@ -8,36 +10,57 @@ if __name__ == '__main__':
 
     players = smash.tournament_show_players('redfox-2018-winter-1', 'domingo-smash-4-singles')
     all_sets = smash.tournament_show_sets('redfox-2018-winter-1', 'domingo-smash-4-singles')
-    brackets = smash.tournament_show_event_brackets('redfox-2018-winter-1', 'domingo-smash-4-singles')
 
     # Para reemplazar los ids con los tags de los jugadores.
     players_and_ids = [[p['tag'], p['entrant_id']] for p in players]
 
     # Elimina sets con DQs.
-    sets = [x for x in all_sets if (x['entrant_1_score'] != -1 and x['entrant_2_score'] != -1)]
-    # Tomamos los jugadores: J1, J2, ganador.
-    sets_played = [[item['entrant_1_id'], item['entrant_2_id'], item['winner_id']] for item in sets]
+    sets = [x for x in all_sets if (x['entrant_1_score'] != -1 and x['entrant_2_score'] != -1 and x['bracket_id'] == '499842')]
+    # Tomamos los jugadores: J1, J2, ganador
+    sets_played_winners = [[item['entrant_1_id'], item['entrant_2_id'], item['winner_id']] for item in sets if "loser" not in item['full_round_text'].lower()]
+    sets_played_losers = [[item['entrant_1_id'], item['entrant_2_id'], item['winner_id']] for item in sets if "loser" in item['full_round_text'].lower()]
 
     #positions = [item['final_placement'] for item in players]
 
     # Reordenamos para quedarnos con perdedor-ganador en todos los sets.
-    for n, _set in enumerate(sets_played):
-        sets_played[n] = list(_set[0:2])
+    for n, _set in enumerate(sets_played_winners):
+        sets_played_winners[n] = list(_set[0:2])
         if _set[2] == _set[0]:
-            sets_played[n].reverse()
+            sets_played_winners[n].reverse()
+
+    for n, _set in enumerate(sets_played_losers):
+        sets_played_losers[n] = list(_set[0:2])
+        if _set[2] == _set[0]:
+            sets_played_losers[n].reverse()
 
     # Sustituímos el id por el tag del jugador.
-    for n, _set in enumerate(sets_played):
+    for n, _set in enumerate(sets_played_winners):
         for m, player in enumerate(_set):
             for p in players_and_ids:
                 if p[1] == int(player):
-                    sets_played[n][m] = p[0]
+                    sets_played_winners[n][m] = p[0]
+                    break
+
+    for n, _set in enumerate(sets_played_losers):
+        for m, player in enumerate(_set):
+            for p in players_and_ids:
+                if p[1] == int(player):
+                    sets_played_losers[n][m] = p[0]
                     break
 
     # Escribimos en un archivo.
     f = open("./Data/data_redfox", 'w')
 
-    for current_set in sets_played:
-        f.write("%s\n" % current_set)
+    for current_set in sets_played_winners:
+        # Añade tags eliminando espacios (para operar mejor en el futuro).
+        f.write(' '.join(str(x).replace(' ', '') for x in current_set))
+        # Añade tier y salto de línea. La tier debería automatizarse en el futuro, dependiendo del torneo.
+        f.write(' B W\n')
+
+    for current_set in sets_played_losers:
+        # Añade tags eliminando espacios (para operar mejor en el futuro).
+        f.write(' '.join(str(x).replace(' ', '') for x in current_set))
+        # Añade tier y salto de línea. La tier debería automatizarse en el futuro, dependiendo del torneo.
+        f.write(' B L\n')
 
     f.close()
